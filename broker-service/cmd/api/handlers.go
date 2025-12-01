a0ce27c0b0208fce03115b2d346642e3e1608e68
kiraberos <kdmytriev@gmail.com>
2025-12-01 21:19:55 +0200

    feat: Implement user authentication handler
    Adds a new HTTP handler for user authentication, allowing
    clients to submit authentication requests. The handler processes
    incoming JSON payloads and communicates with an external
    authentication service.
    
    Changes:
    - Define RequestPayload and AuthPayload structs for JSON
      handling
    - Implement HandleSubmission method to process requests
    - Add authenticate method to handle communication with
      the authentication service
    - Update routes to include new authentication endpoint
    
    This feature is essential for securing access to the API
    and managing user sessions effectively.

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 )
+
+type RequestPayload struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+}
+
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -11,4 +26,78 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayload RequestPayload
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		err := app.errorJSON(w, err)
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	switch requestPayload.Action {
+	case "auth":
+		app.authenticate(w, requestPayload.Auth)
+	default:
+		err := app.errorJSON(w, fmt.Errorf("unknown action"))
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, authPayload AuthPayload) {
+	jsonData, _ := json.MarshalIndent(authPayload, "", "\t")
+
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Error closing response body:", err)
+		}
+	}(response.Body)
+
+	if response.StatusCode == http.StatusUnauthorized {
+		_ = app.errorJSON(w, errors.New("invalid credentials"))
+		return
+	} else if response.StatusCode != http.StatusAccepted {
+		_ = app.errorJSON(w, errors.New("error calling auth service"))
+		return
+	}
+
+	var jsonFromService jsonResponse
+
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	if jsonFromService.Error {
+		_ = app.errorJSON(w, fmt.Errorf("authentication failed: %s", jsonFromService.Message), http.StatusUnauthorized)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Authenticated!"
+	payload.Data = jsonFromService.Data
+
+	_ = app.writeJSON(w, http.StatusAccepted, payload)
 }
